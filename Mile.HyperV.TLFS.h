66367f3a4294317f774b435df1addfe323668695
MouriNaruto <KurikoMouri@outlook.jp>
2023-10-18 23:48:09 +0800

    Create Mile.HyperV.TLFS header file.

---
+++ Mile.HyperV.TLFS.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.HyperV.TLFS.h
+ * PURPOSE:   Definition for Hyper-V Top Level Functional Specification 6.0b
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: MouriNaruto (Kenji.Mouri@Outlook.com)
+ */
+
+#ifndef MILE_HYPERV_TLFS
+#define MILE_HYPERV_TLFS
+
+#include <stdint.h>
+
+#endif // !MILE_HYPERV_TLFS
