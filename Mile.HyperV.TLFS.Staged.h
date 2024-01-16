bb677a42c5ccf7c74724896ae6388cee5666a4a8
MouriNaruto <Kenji.Mouri@outlook.com>
2024-01-16 23:24:14 +0800

    Add Mile.HyperV.TLFS.Staged.

---
+++ Mile.HyperV.TLFS.Staged.h
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.HyperV.TLFS.Staged.h
+ * PURPOSE:   Definition for Hyper-V Top Level Functional Specification 6.0b
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MILE_HYPERV_TLFS_STAGED
+#define MILE_HYPERV_TLFS_STAGED
+
+/* Staged (unused) definitions of Mile.HyperV.TLFS */
+
+#include "Mile.HyperV.TLFS.h"
+
+#endif /* !MILE_HYPERV_TLFS_STAGED */
