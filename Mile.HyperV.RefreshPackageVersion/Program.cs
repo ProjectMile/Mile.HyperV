603c3f12552b9905020648832cadd90a53b8a982
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-14 23:26:09 +0800

    Add Mile.HyperV.RefreshPackageVersion project.

---
+++ Mile.HyperV.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Mile.HyperV.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "Mile.HyperV.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
