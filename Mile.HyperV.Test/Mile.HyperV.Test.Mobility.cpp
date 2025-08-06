d7e6cd0e61a8f1808bc18462e801d58ef1ced56d
MouriNaruto <Kenji.Mouri@outlook.com>
2025-08-06 08:46:03 +0800

    Add Mile.HyperV.Test project.

---
+++ Mile.HyperV.Test/Mile.HyperV.Test.Mobility.cpp
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.HyperV.Test.Mobility.cpp
+ * PURPOSE:    Implementation for Mile.HyperV MobilityOS Test
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Mile.Mobility.Portable.Types.h>
+#include <sal.h>
+
+#include "Mile.HyperV.VMBus.h"
---
+++ Mile.HyperV.Test/Mile.HyperV.Test.Windows.cpp
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:    Mouri Internal Library Essentials
+ * FILE:       Mile.HyperV.Test.Windows.cpp
+ * PURPOSE:    Implementation for Mile.HyperV Windows Test
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+#include <Mile.HyperV.VMBus.h>
+#include <Mile.HyperV.Windows.VMBusPipe.h>
